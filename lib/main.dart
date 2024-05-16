5a5897db8ade52ee5b3facf32a5a8d0f8061d625
sandracriollo34 <criollosandracorina@gmail.com>
2024-05-16 18:44:44 -0500

    agragamos el appbar

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,13 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: Text("MI APP"),
+        ),
+        body: const Center(
+          child:  Text('Hello World!'),
         ),
       ),
     );
